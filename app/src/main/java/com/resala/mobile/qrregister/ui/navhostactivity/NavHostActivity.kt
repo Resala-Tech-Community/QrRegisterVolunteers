00d26a89022f3c5982d8c40f6ece05faf73820ad
Ahmed Elmasry <ahmedsalah.eg70@gmail.com>
2020-02-04 23:27:08 +0200

    login_Screen

--- app/src/main/java/com/resala/mobile/qrregister/ui/navhostactivity/NavHostActivity.kt
+++ app/src/main/java/com/resala/mobile/qrregister/ui/navhostactivity/NavHostActivity.kt
@@ -5,6 +5,8 @@
 
 package com.resala.mobile.qrregister.ui.navhostactivity
 
+import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.resala.mobile.qrregister.R
 import com.resala.mobile.qrregister.shared.ui.activity.BaseActivity
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -13,10 +15,13 @@ import org.koin.android.viewmodel.ext.android.viewModel
 class NavHostActivity : BaseActivity<NavHostActivityVm>() {
 
     override val vm: NavHostActivityVm by viewModel()
-    override var layoutId: Int = R.layout.activity_nav_host
+    override var layoutId: Int = R.layout.login_frag
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutId)
+    }
 
-    override fun doOnCreate() {
-        super.doOnCreate()
+    fun loadFragment(fragment: Fragment) {
 
     }
 
